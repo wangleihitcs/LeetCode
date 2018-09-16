150c2a30d0b36e445da9fa70407cbb15d49f60c9
wanglei <wanglei101100@gmail.com>
2018-09-16 22:16:56 +0800

    2018.09.16

--- medium/5LongestPalindromicSubstring.cpp
+++ medium/5LongestPalindromicSubstring.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <vector>
+#include <math>
 using namespace std;
 
 class Solution {
@@ -77,11 +78,40 @@ public:
         }
         return result;
     }
+    //O(n^2), expand around center
+    string longestPalindrome2(string s) {
+        if(s.size() <= 1) return s;
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = len1 > len2 ? len1 : len2;
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        string result;
+        cout << start << " " << end << " " << endl;
+        for(int i = start; i <= end; i++) {
+            result += s[i];
+        }
+        return result;
+    }
+    int expandAroundCenter(string s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s[L] == s[R]) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+    
     
 };
 
 int main() {
     Solution so;
     string s = "aacdefcaa";
-    cout << so.longestPalindrome1(s) << endl;
+    cout << so.longestPalindrome2(s) << endl;
 }
